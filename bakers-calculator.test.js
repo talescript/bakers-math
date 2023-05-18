80d969e514a83ec0d922abbcec519ab81f850643
talescript <hello.talescript@gmail.com>
2023-05-18 18:27:44 +0200

    Create tests for bakers calculator

---
+++ bakers-calculator.test.js
@@ -0,0 +1,70 @@
+/* eslint-disable */
+const bc = require('./bakers-calculator')
+
+test('300g of water divided over 500g flour to equal 60', () => {
+    expect(bc.ingredientPercentage(300, 500)).toBe(60);
+});
+
+test('750g of water divided over 1000g flour to equal 75', () => {
+    expect(bc.ingredientPercentage(750, 1000)).toBe(75);
+});
+
+test('20g of salt divided over 1000g flour to equal 2', () => {
+    expect(bc.ingredientPercentage(300, 500)).toBe(60);
+});
+
+test('200g of leaven divided over 1000 flour to equal 20', () => {
+    expect(bc.ingredientPercentage(300, 500)).toBe(60);
+});
+
+test('doughWeight has to be higher than zero', () => {
+    expect(bc.ingredientPercentage(300, 0)).toEqual('Error: doughWeight must be higher than zero.');
+});
+
+test('must be numeric values.', () => {
+    expect(bc.ingredientPercentage('300', 500)).toEqual('Error: Both ingredientWeight and doughWeight must be numeric values');
+});
+
+test('must be numeric values.', () => {
+    expect(bc.ingredientPercentage(300, '500')).toEqual('Error: Both ingredientWeight and doughWeight must be numeric values');
+});
+
+test('Must be a number', () => {
+    expect(bc.ingredientPercentage(NaN, 500)).toEqual('Error: Nan');
+});
+
+test('Must be a number', () => {
+    expect(bc.ingredientPercentage(200, NaN)).toEqual('Error: Nan');
+});
+
+test('40% of water with 500g flour to equal 200g', () => {
+    expect(bc.ingredientWeight(40, 500)).toBe(200);
+});
+
+test('2% of salt with 1000g flour to equal 20g', () => {
+    expect(bc.ingredientWeight(2, 1000)).toBe(20);
+});
+
+test('ingredientPercent must be a number', () => {
+    expect(bc.ingredientWeight('300', 0)).toEqual('Error: Both ingredientPercent and doughWeight must be numeric values.');
+});
+
+test('doughWeight must be a number', () => {
+    expect(bc.ingredientWeight(200, '0')).toEqual('Error: Both ingredientPercent and doughWeight must be numeric values.');
+});
+
+test('doughWeight must be a number', () => {
+    expect(bc.ingredientWeight(0.4, 500)).toEqual('Error: The program converts the value into a float');
+});
+
+test('doughWeight must be a higher than zero', () => {
+    expect(bc.ingredientWeight(200, 0)).toEqual('Error: doughWeight must be higher than zero.');
+});
+
+test('doughWeight must be a higher than zero', () => {
+    expect(bc.ingredientWeight(200, NaN)).toEqual('Error: Nan');
+});
+
+test('doughWeight must be a higher than zero', () => {
+    expect(bc.ingredientWeight(NaN, 500)).toEqual('Error: Nan');
+});
